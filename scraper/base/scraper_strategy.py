a9d294cf5303039b4c292ee4d0ee656b204c70f2
afifamaruf <afifallimaruf40@gmail.com>
2025-12-04 11:10:36 +0700

    membuat kode untuk scraper base

---
+++ scraper/base/scraper_strategy.py
@@ -0,0 +1,41 @@
+"""
+File yang berisi class ScraperBase 
+yang berfungsi sebagai interface untuk semua scraper
+
+Name: Afif Alli Ma'ruf
+Date: 2025
+"""
+
+from abc import ABC, abstractmethod
+
+class ScraperBase(ABC):
+
+    @abstractmethod
+    def fetch_listings(self) -> list[dict]:
+        """
+        Return:
+            [
+                {
+                    "title": "...",
+                    "company": "...",
+                    "detail_url": "..."
+                },
+                ...
+            ]
+        """
+        pass
+
+    @abstractmethod
+    def fetch_job_detail(self, url:str) -> dict:
+        """
+        Return:
+            {
+                "title": "...",
+                "company": "...",
+                "location": "...",
+                "salary": "...",
+                "description": "...",
+                "requirements": [...]
+            }
+        """
+        pass
